c6ae8090bef1f453d3ea39a4e6bf887387ccdd3e
professionalowo <martin.brandl1307@gmail.com>
2024-09-08 13:25:39 +0200

    refactored server a bit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::net::Ipv4Addr;
+
 use server::Server;
 pub mod server;
 
 fn main() -> std::io::Result<()> {
-    let server = Server::new();
+    let server = Server::new((Ipv4Addr::new(127, 0, 0, 1), 25565));
     server.run()
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,53 +1,30 @@
 use std::{
     io::{BufRead, BufReader},
-    net::{TcpListener, TcpStream},
-    sync::Arc,
+    net::{TcpListener, TcpStream, ToSocketAddrs},
     thread::{self, JoinHandle},
     time::Duration,
 };
 
-fn configure_stream(stream: &TcpStream) -> std::io::Result<()> {
-    let duration = Some(Duration::from_secs(2));
-
-    stream.set_read_timeout(duration)?;
-    stream.set_write_timeout(duration)?;
-
-    stream.set_nodelay(true)?;
-    stream.set_nonblocking(false)?;
-    Ok(())
-}
-
-fn handle_client(stream: TcpStream) -> std::io::Result<()> {
-    println!("Accepting connection from {}", stream.local_addr()?.ip());
-
-    configure_stream(&stream)?;
-
-    let mut reader = BufReader::new(stream);
-    let mut buf: Vec<u8> = Vec::with_capacity(250);
-    while let Ok(n) = reader.read_until(20, &mut buf) {
-        if n == 0 {
-            break;
-        }
-    }
-    println!("{:?}", buf);
-    Ok(())
-}
-
-pub struct Server {
+pub struct Server<A>
+where
+    A: ToSocketAddrs,
+{
     stream_handles: Vec<JoinHandle<std::io::Result<()>>>,
+    address: A,
 }
 
-impl Server {
-    pub fn new() -> Self {
+impl<A: ToSocketAddrs + std::marker::Send + 'static> Server<A> {
+    pub fn new(address: A) -> Self {
         Self {
             stream_handles: Vec::new(),
+            address,
         }
     }
 
     pub fn run(mut self) -> std::io::Result<()> {
-        let listener = TcpListener::bind("127.0.0.1:25565")?;
+        let listener = TcpListener::bind(self.address)?;
         for stream in listener.incoming().flatten() {
-            let handle = thread::spawn(|| handle_client(stream));
+            let handle = thread::spawn(|| Self::handle_client(stream));
             self.stream_handles.push(handle);
         }
 
@@ -55,5 +32,35 @@ impl Server {
             handle.join().ok();
         }
         Ok(())
+    }
+
+    fn handle_client(stream: TcpStream) -> std::io::Result<()> {
+        println!("Accepting connection from {}", &stream.local_addr()?.ip());
+        let s = Self::configure_stream(&stream)?;
+        drop(stream);
+
+        let mut reader = BufReader::new(s);
+        let mut buf: Vec<u8> = Vec::with_capacity(250);
+        while let Ok(n) = reader.read_until(20, &mut buf) {
+            //is only reached after stream closes
+            if n == 0 {
+                break;
+            }
+        }
+        println!("{:?}", buf);
+        Ok(())
+    }
+
+    fn configure_stream(stream: &TcpStream) -> std::io::Result<TcpStream> {
+        let read_timeout_duration = Duration::from_secs(5);
+        let write_timeout_duration = Duration::from_secs(1);
+
+        let s = stream.try_clone()?;
+        s.set_read_timeout(Some(read_timeout_duration))?;
+        s.set_write_timeout(Some(write_timeout_duration))?;
+
+        s.set_nodelay(false)?;
+        s.set_nonblocking(false)?;
+        Ok(s)
     }
 }
